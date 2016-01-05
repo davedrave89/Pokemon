b00f5b4651ca72b437b9595c72533a570f03b91c
Dave <davedrave@hotmail.com>
2016-01-05 00:27:36 +0000

    Post Merge Salvage Attempt
    I appear to have fucked up the code by doing a merge (prompted to by
    GitHub) in heinsight there should have been no merge, I will be more
    careful in future with this clandestine technology!

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from move import *
 from assets import *
 import random
 
-<<<<<<< HEAD
-
+#Declare classes
 punch = Move("Punch", 10, 20, 20)
 kick = Move("Kick", 10, 20, 20)
 
@@ -11,39 +10,29 @@ moves = [punch, kick]
 
 pokemon = Pokemon("Claire", 10, moves)
 
-result = raw_input("Please select move: \n1. Kick\n2. Punch\n")
-result = int(result) # parse string result to int
-
-result -= 1 #decrement result because the moves array is 0 based
-
-print(pokemon.Moves[result].Name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
-print(random.randint(0,9))
-=======
-#Declare classes
-move = Move("Punch", 10, 20, 20)
-pokemon = Pokemon("Charmander", 10, move)
 user = User("Claire", 100, 1000)
 
 #Start Game
 
 while (user.healing > 0):
-	result = raw_input("Please select move: \n1. Kick\n2. Punch\n")
-	print(pokemon.Move.Name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
-	hit = random.randint(0,100)
+    result = raw_input("Please select move: \n1. Kick\n2. Punch\n")
+    result = int(result) # parse string result to int
+    result -= 1
+    print(pokemon.Moves[result].Name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
+    hit = random.randint(0,100)
 
-	print(pokemon.GetName())
-	print(user.GetName())
-	print(user.Score())
-	healing = user.Healing()
-	print("You have been hit: ", hit)
+    print(pokemon.GetName())
+    print(user.GetName())
+    print(user.Score())
+    healing = user.Healing()
+    print "You have been hit: ", hit
 
-	# Function that will calculate the score of player
-	def calculate_score(healing, score):
-		total = healing - score
-		user.healing = total
-		print("Your current health is: ", user.healing)
+    # Function that will calculate the score of player
+def calculate_score(healing, score):
+	total = healing - score
+	user.healing = total
+	print("Your current health is: ", user.healing)
 
-	calculate_score(healing, hit)
->>>>>>> origin/master
+calculate_score(healing, hit)
 
 print("Game Over")

f0ba88d7c7d53f4b88995b691fb88cb6dffd0c96
Dave <davedrave@hotmail.com>
2016-01-10 23:56:31 +0000

    Variable Rename
    1.main.py - Renamed result to move_choice to be more specific to its
    purpose

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ while (user.health > 0):
     for index, move in enumerate(pokemon.moves):
         print str(index + 1) + "." + move.name
 
-    result = raw_input()
-    result = int(result) # parse string result to int
-    result -= 1
-    print(pokemon.name + " used " + pokemon.moves[result].name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
+    move_choice = raw_input()
+    move_choice = int(move_choice) # parse string result to int
+    move_choice -= 1
+
+    print(pokemon.name + " used " + pokemon.moves[move_choice].name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
     hit = random.randint(0,100)
 
     print(user.score)

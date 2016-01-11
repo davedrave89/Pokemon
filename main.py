9e527dc019093241ab9ad50ff616f8a57ae3a37e
Dave <davedrave@hotmail.com>
2016-01-11 00:36:00 +0000

    Added move damage
    Player now recieves damage as a result of a move being dealt.
    
    1. move.py - renamed Range property to range and added a setter.
    
    2. move.py renamed Name property to name
    
    3.main.py - introduced a list into the range property going into pokemon
    instead of a single number.
    
    4. added logic that deals damage within that range to the user

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from assets import *
 import random
 
 #Declare classes
-punch = Move("Punch", 10, 20, 20)
-kick = Move("Kick", 10, 20, 20)
+punch = Move("Punch", 10, [18,25], 20)
+kick = Move("Kick", 10, [10,35], 20)
 
 moves = [punch, kick]
 
@@ -23,16 +23,23 @@ while (user.health > 0):
     for index, move in enumerate(pokemon.moves):
         print str(index + 1) + "." + move.name
 
+    #get users choice
     move_choice = raw_input()
     move_choice = int(move_choice) # parse string result to int
     move_choice -= 1
 
-    print(pokemon.name + " used " + pokemon.moves[move_choice].name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
-    hit = random.randint(0,100)
+    #reuse move_choice, now to store the move itself (neater than referring to it WITHIN pokemon.moves)
+    move_choice = pokemon.moves[move_choice]
 
-    print(user.score)
-    healing = user.health
-    print "You have been hit: ", hit
+
+    #damage dealt will be a random amount within the range specified by the move
+    damage = random.randint(move_choice.range[0],move_choice.range[1])
+
+    print(pokemon.name + " used " + move_choice.name + ", causing " + str(damage) + " damage.") #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
+
+    user.health = user.health - damage
+
+    print(user.name + "'s health is now " + str(user.health))
 
     # Function that will calculate the score of player
 def calculate_score(healing, score):

fadb884b1cd35eb1921f5f04e7fbdd0b6be0a6e0
clairethebear <claire.oubelaid@gmail.com>
2016-02-20 21:05:56 +0000

    Random code, preparation for more code...

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+
+
+
+
 from move import *
 from assets import *
 import random
@@ -12,6 +16,14 @@ pokemon = Pokemon("Claire", 10, moves)
 
 user = User("Claire", 100, 1000)
 
+    # Function that will calculate the score of player
+def calculate_score(healing, score):
+	score = healing - score
+	health = score
+	print("Your current health is: ", user.health)
+	
+	return score, health
+
 #Start Game
 
 while (user.health > 0):
@@ -20,17 +32,15 @@ while (user.health > 0):
     result -= 1
     print(pokemon.name + " used " + pokemon.moves[result].name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
     hit = random.randint(0,100)
+        
+    print calculate_score(user.health, hit)
 
     print(user.score)
     healing = user.health
     print "You have been hit: ", hit
 
-    # Function that will calculate the score of player
-def calculate_score(healing, score):
-	total = healing - score
-	user.healing = total
-	print("Your current health is: ", user.healing)
 
-calculate_score(healing, hit)
+
+
 
 print("Game Over")

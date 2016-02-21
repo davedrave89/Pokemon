b5417c1b0228e269979238503fe548c652b481f3
clairethebear <claire.oubelaid@gmail.com>
2016-02-21 14:01:07 +0000

    Updated class player and computer to inherit from pokemon. Other game logic added and improved

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-
-
-
-
 from move import *
 from assets import *
 import random
@@ -10,37 +6,49 @@ import random
 punch = Move("Punch", 10, 20, 20)
 kick = Move("Kick", 10, 20, 20)
 
-moves = [punch, kick]
+moves = ['Tackle', 'Tail whip']
 
-pokemon = Pokemon("Claire", 10, moves)
+pokemon = Pokemon("Squirtle", 1000, moves)
+
+# computer_player = Computer_player("Claire", 100, 1000, "Squirtle", 1000, moves)
 
 user = User("Claire", 100, 1000)
+computer = User("Computer", 100, 1000)
+
+def hit():
+    return random.randint(0,100)
 
     # Function that will calculate the score of player
-def calculate_score(healing, score):
-	score = healing - score
-	health = score
-	print("Your current health is: ", user.health)
+def calculate_score(healing, hit, score):
+	computer.health = pokemon.health - hit
+	user.score += 20
+	print(pokemon.name +  " health is: ", pokemon.health)
 	
-	return score, health
+	return hit, user.health, user.score
+
+
+def computer_hit():
+    computer_move = random.randint(0, len(moves)-1)
+    print computer.name + " used " + moves[computer_move]
+    computer.score += 20
+    
 
 #Start Game
-
-while (user.health > 0):
-    result = raw_input("Please select move: \n1. Kick\n2. Punch\n")
-    result = int(result) # parse string result to int
-    result -= 1
-    print(pokemon.name + " used " + pokemon.moves[result].name) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
-    hit = random.randint(0,100)
+# while (user.health > 0):
+#     print "***********************************Next Move ******************************"
+#     result = raw_input("Please select move: \n1. " + pokemon.moves[0] + "\n2. " + pokemon.moves[1] + "\n")
+#     result = int(result) # parse string result to int
+#     result -= 1
+#     print(pokemon.name + " used " + pokemon.moves[result]) #printing name within pokemon to show that an instantiation of the Move class has been passed into the pokemon object
+#     hit = hit()
         
-    print calculate_score(user.health, hit)
-
-    print(user.score)
-    healing = user.health
-    print "You have been hit: ", hit
+#     print calculate_score(user.health, hit, user.score)
 
 
+#     print(user.score)
+#     healing = user.health
+#     print "You have been hit: ", hit
 
+# print("Game Over")
 
-
-print("Game Over")
+computer_hit()
--- move.py
+++ move.py
@@ -1,4 +1,4 @@
-from assets import GameModel
+from assets import *
 
 class Pokemon(GameModel):
     """Pokemon class represents pokemon which will be used by the Player/Computer to do battle"""
@@ -55,3 +55,14 @@ class Message():
 
     def Health():
         pass
+
+# Adding player that inherits a pokemon object, added to move.py becuase of cirular dependancy errors 
+# class Computer_player(Computer, Pokemon):
+#     """Computer_player class will be opponent to user with pokemon"""
+#     def __init__(self, name, score, health, name, health, moves):
+#         pass
+
+# class Player_player(Player, Pokemon):
+#     """Player class that is controlled by the user with a pokemon"""
+#     def __init__(self, name, score, health, moves):
+#         pass

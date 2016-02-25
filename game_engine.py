cbb7633694d8f3d3ff745a7f6d806590b0ac1b49
davedrave89 <davedrave@hotmail.com>
2016-02-25 22:36:58 +0000

    Adding game_engine class which will handle game logic, such as when the game ends.

---
+++ game_engine.py
@@ -0,0 +1,5 @@
+class game_engine():
+    """game_engine class will contain any game logic or attributes to drive the flow of the game and its rules"""
+
+    def game_is_over(self):
+        return False
